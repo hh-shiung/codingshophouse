21c35d812ba4bfb6d8ed7a8ec4ccec739e9a9daf
ytbryan <bryan@shoppink.me>
2015-06-22 15:58:10 +0800

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,24 @@
 class User < ActiveRecord::Base
 
-  after_initialize :defaults
-
-  def defaults
-    self.name ||= 'John Doe'
-    self.jobs ||= 'Your job here'
-    self.facebook ||= '#'
-    self.github ||= '#'
-    self.website ||= '#'
-    self.picture ||= 'johndoe.png'
-    self.batch_number ||= '1'
-    self.role ||= 'student'
-    # self.approved = false if self.approved.nil?
-
-  end
+  #ryan, use migration to set default value
+  #good try
+  # add_column :users, :name, :string, :default => 'John Doe'
+  #why use migration? Because defaults only need to run once. 
+  #http://stackoverflow.com/questions/6167994/assigning-default-value-while-creating-migration-file
+  
+  # after_initialize :defaults
+  # 
+  # def defaults
+  #   self.name ||= 'John Doe'
+  #   self.jobs ||= 'Your job here'
+  #   self.facebook ||= '#'
+  #   self.github ||= '#'
+  #   self.website ||= '#'
+  #   self.picture ||= 'johndoe.png'
+  #   self.batch_number ||= '1'
+  #   self.role ||= 'student'
+  #   # self.approved = false if self.approved.nil?
+  # 
+  # end
 
 end
